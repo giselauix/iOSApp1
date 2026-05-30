d673fba0709a67a2fe07724d4deb95f85b792eee
giselauix <Amalia.Bonilla@triosstudent.com>
2026-05-30 14:05:04 -0400

    Initial Commit

---
+++ TimHortonsRun/TimHortonsRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimHortonsRunApp.swift
+//  TimHortonsRun
+//
+//  Created by Amalia on 2026-05-30.
+//
+
+import SwiftUI
+
+@main
+struct TimHortonsRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
